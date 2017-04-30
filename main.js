989d3a1ebff9859567d60ed7ffb73699cdfc981c
44ust14 <ustym.hanyk@ukr.net>
2017-04-30 14:57:16 +0300

    last

---
+++ main.js
@@ -0,0 +1,76 @@
+
+var x; /* Оголошення зміної*/
+
+x = 5 ;
+
+var y = 10; /* Оголошення зміної та ініціанідізація*/
+
+var resultAdd = x + y;
+
+var resultSub = x - y;
+
+var resultMult = x * y;
+
+var resultDiv = x/y;
+console.log('Додавання: ', resultAdd);
+console.log('Віднімання: ', resultSub);
+console.log('Множення: ', resultMult);
+console.log('Ділення: ', resultDiv);
+
+var firstNumber = prompt('Enter first Number');
+var secondNumber = prompt('Enter second Number');
+
+console.log(firstNumber);
+console.log(secondNumber);
+
+var Add = firstNumber + secondNumber;
+var Sub = firstNumber - secondNumber;
+var Mult = firstNumber * secondNumber;
+var Div = firstNumber / secondNumber;
+
+console.log(Add);
+console.log(Sub);
+console.log(Mult);
+console.log(Div);
+
+var firstNumber = Number(prompt('Enter first Number'));
+var secondNumber = Number(prompt('Enter second Number'));
+
+console.log(firstNumber);
+console.log(secondNumber);
+
+var add = firstNumber + secondNumber;
+var sub = firstNumber - secondNumber;
+var mult = firstNumber * secondNumber;
+var div = firstNumber / secondNumber;
+var mod = firstNumber % secondNumber;
+var inc = ++firstNumber;
+var dec = --secondNumber;
+
+
+
+
+console.log('Додавання: ', add);
+console.log('Віднімання: ', sub);
+console.log('Множення: ', mult);
+console.log('Ділення: ', div);
+console.log('Остача: ', mod);
+console.log('+1: ', inc);
+console.log('-1: ', dec);
+
+
+
+
+
+
+
+var txt1 = 'My first txt ';
+var txt2 = 'My second txt ';
+
+document.write(txt1+txt2);
+
+
+
+var usertxt = prompt ('Your text');
+
+document.write('<br>' + usertxt);
